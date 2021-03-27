2e1a69e8bcd72edc7876cf6fba6e0404b35beb13
MasloJePotravina <ondrejosboss@gmail.com>
2021-03-27 19:40:40 +0100

    Added test file, created test classes

---
+++ src/mathlib_tests.py
@@ -0,0 +1,40 @@
+## @file mathlib_tests.py
+#  @author Ondrej Kováč
+#  @date 27.3.2021
+#  @brief Unit tests for mathematical library "mathlib"
+
+import unittest
+import mathlib
+
+## @brief Tests for addition
+class TestAddition(unittest.TestCase):
+    #TODO
+
+## @brief Tests for subtraction
+class TestSubtraction(unittest.TestCase):
+    #TODO
+
+## @brief Tests for multiplication
+class TestMultiplication(unittest.TestCase):
+    #TODO
+
+## @brief Tests for division
+class TestDivision(unittest.TestCase):
+    #TODO
+
+## @brief Test factorial
+class TestFactorial(unittest.TestCase):
+    #TODO
+
+## @brief Test exponent
+class TestExponent(unittest.TestCase):
+    #TODO
+
+## @brief Test root
+class TestRoot(unittest.TestCase):
+    #TODO
+
+if __name__ == unittest:
+    unittest.main()
+
+# End of file mathlib_tests.py        
