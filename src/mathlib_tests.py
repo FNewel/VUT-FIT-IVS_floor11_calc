1830716827b769887faa845cbaf9a76f64f4bc12
MasloJePotravina <ondrejosboss@gmail.com>
2021-03-27 19:44:28 +0100

    Fixed typos in comments

--- src/mathlib_tests.py
+++ src/mathlib_tests.py
@@ -22,19 +22,19 @@ class TestMultiplication(unittest.TestCase):
 class TestDivision(unittest.TestCase):
     #TODO
 
-## @brief Test factorial
+## @brief Tests for factorial
 class TestFactorial(unittest.TestCase):
     #TODO
 
-## @brief Test exponent
+## @brief Tests for exponent
 class TestExponent(unittest.TestCase):
     #TODO
 
-## @brief Test root
+## @brief Tests for root
 class TestRoot(unittest.TestCase):
     #TODO
 
-if __name__ == unittest:
+if __name__ == '__main__':
     unittest.main()
 
 # End of file mathlib_tests.py        
