6ed62910300f4ae46647afc2921a15af2852b25c
MasloJePotravina <ondrejosboss@gmail.com>
2021-04-26 22:09:14 +0200

    Added optimized mathlib and profiling (PGO)

--- src/mathlib.py
+++ src/mathlib.py
@@ -1,51 +1,42 @@
 ## @file mathlib.py
 #  @author Ondrej Kováč
+#  @author Martin Talajka (optimization)
 #  @date 13.4.2021
 #  @brief Math library defining math operations (+,-,*,/,!,^,nth_root, RNG)
 
-
 from random import randint
-
 
 ## @brief Sum of 2 numbers
 # @return a + b
 def add(a,b):
-    return a+b
+    return a + b
 
 ## @brief Subtraction of 2 numbers
 # @return a - b
 def sub(a,b):
-    return a-b
+    return a - b
 
 ## @brief Product of 2 numbers
 # @return a * b
 def mul(a,b):
-    return a*b
+    return a * b
 
 ## @brief Division of 2 numbers
 # @exception ZeroDivisionError if @p b is equal to 0
 # @return a / b
 def div(a,b):
-    if b == 0:
-        raise ZeroDivisionError
-
-    return a/b
+    if b != 0: return a / b
+    raise ZeroDivisionError
 
 ## @brief Factorial of a whole number
 # @exception ValueError if @p n is negative
 # @exception TypeError if @p n is a fraction
 # @return n!
 def fact(n):
-    if n < 0:
-        raise ValueError
-    if n % 1 != 0:
-        raise TypeError 
-
-    n = int(n)
+    if n < 0: raise ValueError
+    if n % 1 != 0: raise TypeError 
     result = 1
-    for i in range(1, n+1):
-        result *= i
-
+    for i in range(1, int(n) + 1): result *= i
     return result
 
 ## @brief Exponentiation of a base @p b by an exponent @p n 
@@ -53,13 +44,9 @@ def fact(n):
 # @exception TypeError if @p n is a fraction
 # @return b^n
 def exp(b,n):
-    if n < 0:
-        raise ValueError
-    if n % 1 != 0:
-        raise TypeError 
-    
-    n = int(n)
-    return (b ** n)
+    if n < 0: raise ValueError
+    if n % 1 != 0: raise TypeError 
+    return (b ** int(n))
 
 ## @brief Finding @p n th root of number @p x
 # @exception ZeroDivisionError if @p n is zero
@@ -68,34 +55,20 @@ def exp(b,n):
 # @exception ValueError if @p n is even and @p x is negative
 # @return x^(1/n)
 def root(n,x):
-    if n == 0:
-        raise ZeroDivisionError
-    if n < 0:
-        raise ValueError
-    if n % 1 != 0:
-        raise TypeError
-    if (n % 2 == 0) and (x < 0):
-        raise ValueError
-
-    if (n % 2 == 1) and (x < 0):
-        x = abs(x)
-        return -(x ** (1/n))
-    else:
-        return x ** (1/n)
+    if n == 0: raise ZeroDivisionError
+    if n < 0 or ((n % 2 == 0) and (x < 0)): raise ValueError
+    if n % 1 != 0: raise TypeError
+    if (n % 2 == 1) and (x < 0): return -(abs(x) ** (1/n))
+    return x ** (1/n)
 
 ## @brief Producing a random number in a given range
 # @exception ValueError if @p x == 0
 # @exception TypeError if @p x is a fraction
 # @return Random number from range <0,x>
 def rng(x):
-    if x == 0:
-        raise ValueError
-    if (x % 1 != 0):
-        raise TypeError
-
-    if x > 0:
-        return randint(0,x)
-    if x < 0:
-        return randint(x,0)
+    if x == 0: raise ValueError
+    if (x % 1 != 0): raise TypeError
+    if x > 0: return randint(0,x)
+    return randint(x,0)
 
 #End of file mathlib.py
--- src/profiling.py
+++ src/profiling.py
@@ -1,42 +1,35 @@
 ## @file profiling.py
 #  @author Ondrej Kováč
+#  @author Martin Talajka (optimization)
 #  @date 13.4.2021
 #  @brief Profiling file used for analysis and discovery of inefficiencies in math functions from mathlib.py
 
 
-import mathlib
-import sys
+from mathlib import add, exp, mul, div, exp, sub, root
+from sys import stdin
 
 #Read stdin into an array
-num_arr = []
-for line in sys.stdin:
-    for num in line.split():
-        num = int(num)
-        num_arr.append(num)
-
+num_arr = [int(num) for line in stdin for num in line.split()]
 
 #Create sums
-num_amount = len(num_arr)
-sum_xi = 0
-sum_xi_raised_2 = 0
-for num in num_arr:
-    sum_xi = mathlib.add(sum_xi, num)
-    sum_xi_raised_2 = mathlib.add(sum_xi_raised_2, mathlib.exp(num, 2))
+num_amount, sum_xi, sum_xi_raised_2 = len(num_arr), 0, 0
+
+for num in num_arr: sum_xi, sum_xi_raised_2 = add(sum_xi, num), add(sum_xi_raised_2, exp(num, 2))
 
 #Overline x
-overline_x = mathlib.mul(mathlib.div(1,num_amount), sum_xi)
+overline_x = mul(div(1,num_amount), sum_xi)
 
 #result = N*overlineX^2
-result = mathlib.mul(num_amount, mathlib.exp(overline_x, 2))
+result = mul(num_amount, exp(overline_x, 2))
 
 #result Parentheses
-result = mathlib.sub(sum_xi_raised_2, result)
+result = sub(sum_xi_raised_2, result)
 
 #result = 1\(N-1) * Parentheses
-result = mathlib.mul(mathlib.div(1, mathlib.sub(num_amount, 1)), result)
+result = mul(div(1, sub(num_amount, 1)), result)
 
 #result = sqr_root(1\(N-1) * Parentheses)
-result = mathlib.root(2, result)
+result = root(2, result)
 
 #Print the result
 print(result)
